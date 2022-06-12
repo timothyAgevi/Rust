79e749439189414531383a76683f25df4aa3536a
timothyAgevi <timothyagevi@gmail.com>
2022-06-12 12:54:23 +0300

    install nightly rust toolkit version

--- mars_calc/src/main.rs
+++ mars_calc/src/main.rs
@@ -1,5 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world!");//macros:call ending with '!',receives variable number of args
+//macros:rust code that writes other rust code
+    calculate_weight_on_mars(100.0);
 }
 
 fn calculate_weight_on_mars(weight:f32) -> f32{
