bcf97c98b26c8a1309af6f3be422932cab7d7666
timothyAgevi <timothyagevi@gmail.com>
2022-06-12 18:24:17 +0300

    add struct defination

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    //instance of server
+    let server= Server::new("127.0.0.1.8080");
+    server.run();
+}
+//struct defination
+struct Server{
+    addr:String,
 }
