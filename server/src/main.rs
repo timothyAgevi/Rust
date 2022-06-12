bb246a06b4458623d27aecba4f86e3df4b99266e
timothyAgevi <timothyagevi@gmail.com>
2022-06-12 18:56:08 +0300

    add struct defination and impl

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     //instance of server
-    let server= Server::new("127.0.0.1.8080");
+    let server= Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
 //struct defination
@@ -11,4 +11,14 @@ struct Server{
 impl Server{
   // struc have 2 functionalities:1method or 2.associated function/static function e.g new() 
   //methods : always take 1st parameter of self(instance of struc method is bn called)
+  //:: to acess associated functions
+  fn new(addr:String)->Self{
+    Self{
+        addr
+    }
+  }
+  //run method
+  fn run(self){// run takes ownership of struc instance
+    
+  }
 }
