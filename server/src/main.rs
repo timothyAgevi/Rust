3ce05afe3fcdc468219c6a74431663b7e32b1865
timothyAgevi <timothyagevi@gmail.com>
2022-06-13 11:12:55 +0300

    add server.rs

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,4 @@
+use http::request::Request;
 use server::Server;
 fn main() {
 //Request variables
@@ -10,41 +11,20 @@ fn main() {
     let server= Server::new("127.0.0.1:8080".to_string());//convert from string literal/slice to String
     server.run();
 }
-mod server {
 
-
-//struct defination
- pub struct Server{
-    addr:String,
-}
-//struct implementation
- impl Server{
-  // struc have 2 functionalities:1method or 2.associated function/static function e.g new() 
-  //methods : always take 1st parameter of self(instance of struc method is bn called)
-  //:: to acess associated functions
- pub fn new(addr:String)->Self{
-    Self{
-        addr
-    }
-  }
-  //run method
- pub fn run(self){// run takes ownership of struc instance
-    println!("Server  Listening on {}" ,self.addr);
-  }
-}
-
-}
 mod http{
+  
 //request
-mod request{
-struct Request{
+ pub mod request{
+  use super::method::Method;
+ pub struct Request{
     path:String,
     query_string:Option<String>,
-    method:super ::method::Method,
+    method:Method,
   }
 }
 //enum
-mod method{
+ pub mod method{
  pub enum Method{
     GET,
     DELETE,
---
+++ server/src/server.rs
@@ -0,0 +1,19 @@
+//struct defination
+pub struct Server{
+    addr:String,
+}
+//struct implementation
+ impl Server{
+  // struc have 2 functionalities:1method or 2.associated function/static function e.g new() 
+  //methods : always take 1st parameter of self(instance of struc method is bn called)
+  //:: to acess associated functions
+ pub fn new(addr:String)->Self{
+    Self{
+        addr
+    }
+  }
+  //run method
+ pub fn run(self){// run takes ownership of struc instance
+    println!("Server  Listening on {}" ,self.addr);
+  }
+}
